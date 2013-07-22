3b4239822adfc96a8e78770efed4f2b7c09717f4
arlogn <claudio@bastardi.net>
2013-07-22 02:02:36 +0200

    Share video from youtube

--- data/getdata.js
+++ data/getdata.js
@@ -1,14 +1,14 @@
 var videoData = function () {
-	if (!document.getElementById('content')) {
-     console.log("Easyshare: video data not found.");
-     return false;
-   }
-	var data = {}, c = document.getElementById('content');
-   data.url   = c.querySelector('[itemprop="url"]').getAttribute('href');
-	data.title = c.querySelector('[itemprop="name"]').getAttribute('content');
-	data.desc  = c.querySelector('[itemprop="description"]').getAttribute('content');
-	data.thumb = c.querySelector('[itemprop="thumbnailUrl"]').getAttribute('href');
-	return data;
+  if (!document.getElementById('content')) {
+    console.log("Easyshare: video data not found.");
+    return false;
+  }
+  var data = {}, c = document.getElementById('content');
+  data.url   = c.querySelector('[itemprop="url"]').getAttribute('href');
+  data.title = c.querySelector('[itemprop="name"]').getAttribute('content');
+  data.desc  = c.querySelector('[itemprop="description"]').getAttribute('content');
+  data.thumb = c.querySelector('[itemprop="thumbnailUrl"]').getAttribute('href');
+  return data;
 }
 
 self.on('click', function(node) {
@@ -26,18 +26,18 @@ self.on('click', function(node) {
   } else {
   	 var vd = videoData();
   	 if (vd) {
-  	 	data.node = '<img id="thumb" src="' + vd.thumb + '">';
-  	 	data.title = vd.title;
-  	 	data.text = vd.desc;
-  	 	data.videoUrl = vd.url;
+      data.node = '<img id="thumb" src="' + vd.thumb + '">';
+      data.title = vd.title;
+      data.text = vd.desc;
+      data.videoUrl = vd.url;
   	 } else {
-  	 	data.node = '<p id="thumb" class="warning">No thumbnail and description.<br>' +
+      data.node = '<p id="thumb" class="warning">No thumbnail and description.<br>' +
   	 	            'Video data not found.</p>';
   	   data.title = document.title;
   	   data.text = "";
   	   data.videoUrl = data.url;
   	 }
-  	 data.video = true;
+    data.video = true;
   }
   
   self.postMessage(data);
