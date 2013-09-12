e5e2939231492e4a72ca5143455eb1600b5f5e75
arlogn <claudio@bastardi.net>
2013-09-12 19:21:15 +0200

    new feature - share selection

---
+++ data/data.js
@@ -0,0 +1,54 @@
+var videoData = function () {
+  var c = document.getElementById('content');
+  if (!c) {
+    console.log("Easyshare: video data not found.");
+    return false;
+  }
+  var d = {};
+  d.url   = c.querySelector('[itemprop="url"]').getAttribute('href');
+  d.title = c.querySelector('[itemprop="name"]').getAttribute('content');
+  d.desc  = c.querySelector('[itemprop="description"]').getAttribute('content');
+  d.thumb = c.querySelector('[itemprop="thumbnailUrl"]').getAttribute('href');
+  return d;
+};
+
+self.on('click', function (node, data) {
+  var d = { url: document.URL };
+  
+  switch (data) {
+    case 'image' :
+      d.node = '<img id="datanode" width="' + node.clientWidth + 
+               '" height="' + node.clientHeight + '" src="' + node.src + '">';
+      d.title = document.title; 
+      d.text = window.getSelection().toString();
+      d.video = false;
+      d.videoUrl = 'none';
+      break;
+    case 'video' :
+      var vd = videoData();
+      if (vd) {
+        d.node = '<img id="datanode" src="' + vd.thumb + '">';
+        d.title = vd.title;
+        d.text = vd.desc;
+        d.videoUrl = vd.url;
+      } else {
+        d.node = '<p id="datanode" class="warning">No thumbnail and description.<br>' +
+                 'Video data not found but you can share anyway this video.</p>';
+        d.title = document.title;
+        d.text = "";
+        d.videoUrl = data.url;
+      }
+      d.video = true;
+      break;
+    case 'selection' :    	
+      d.node = '<p id="datanode" class="warning">No image. ' +
+               'You have chosen to share only selected text.</p>';
+      d.title = document.title; 
+      d.text = window.getSelection().toString();
+      d.video = false;
+      d.videoUrl = 'none';  
+      break;
+  }
+  
+  self.postMessage(d);
+});
