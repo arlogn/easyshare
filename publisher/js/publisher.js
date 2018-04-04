21744bb6b0d716b4a920ced8297c2936e5780732
arlogn <claudio@bastardi.net>
2018-04-04 20:10:51 +0200

    Refactor publisher.js
    Now some functionality are handled by bootsrap markdown, a new class for the ajax based communication is introduced and bootstrap-select is removed

--- publisher/js/publisher.js
+++ publisher/js/publisher.js
@@ -1,269 +1,169 @@
-/*jshint esversion: 6 */
+/*jshint esversion: 6*/
 
-function onStorageError(error) {
-  console.log(`Storage: ${error}`);
+const $editor = $("#editor");
+
+function onError(error) {
+    $editor
+        .val(error)
+        .css("color", "red");
+
+    console.log(error);
 }
 
-function onDiasporaError(error, action) {
-  notify(`(${action}) ${error}`, "error");
-  $("#send").prop("disabled", true).text("Failure!");
-  console.log(`${action}: ${error}`);
+function onSuccess(message) {
+    $editor
+        .val(message)
+        .data("markdown")
+        .enableButtons("all");
 }
 
-function notify(message, icon) {
-  const iconUrl = "icons/" + icon + ".png";
-  browser.notifications.create({
-    "type": "basic",
-    "iconUrl": browser.extension.getURL(iconUrl),
-    "title": "Publisher Everywhere",
-    "message": message
-  });
-}
+function updateAspectsDropdown(aspects, notify = false) {
+    $editor
+        .data("markdown")
+        .updateDropdown(aspects);
 
-function updateAspectsSelector(aspects) {
-  $(".selectpicker option:gt(1)").remove();
-  $.each(aspects, (key, value) => {
-    $(".selectpicker").append($("<option>", {
-      value: value.id,
-      text: value.name
-    }));
-  });
-  $(".selectpicker").selectpicker("refresh");
-}
-
-function diasporaSignIn(url, username, password) {
-  const tokenPattern = /<input[^>]* name="authenticity_token"[^>]* value="(.*)"/,
-    failPattern = /<body[^>]*page-sessions[^>]*>/i;
-
-  let deferred = $.Deferred();
-
-  $.get({
-      url: url + "/users/sign_in"
-    })
-    .then(data => {
-        let match = data.match(tokenPattern);
-        if (match) {
-          $.post({
-              url: url + "/users/sign_in",
-              data: {
-                "user[username]": username,
-                "user[password]": password,
-                "user[remember_me]": 1,
-                "utf8": "✓",
-                "authenticity_token": match[1]
-              }
-            })
-            .then(data => {
-                // Try to catch a login error
-                if (failPattern.test(data)) {
-                  deferred.resolve({
-                    error: "Invalid username or password"
-                  });
-                } else {
-                  match = data.match(tokenPattern);
-                  deferred.resolve({
-                    token: match[1]
-                  });
-                }
-              },
-              error => {
-                deferred.reject(error.statusText);
-              });
-        } else {
-          deferred.resolve({
-            error: "Authenticity token not found"
-          });
-        }
-      },
-      error => {
-        deferred.reject(error.statusText);
-      });
-
-  return deferred;
-}
-
-function diasporaGetAspects(url) {
-  $.get({
-      url: url + "/bookmarklet"
-    })
-    .done(data => {
-      let match = data.match(/"aspects":(\[{.*}\]),"services/);
-      if (match) {
-        let userAspects = JSON.parse(match[1]);
-        $.each(userAspects, (index, value) => {
-          delete value.selected;
-        });
-        browser.storage.local.set({
-            aspects: userAspects
-          })
-          .then(() => {
-            updateAspectsSelector(userAspects);
-            notify("Aspects selector updated", "success");
-          });
-      } else {
-        onDiasporaError("Aspects not found", "GetAspects");
-      }
-    })
-    .fail(error => {
-      onDiasporaError(error.statusText, "GetAspects");
-    });
-}
-
-function diasporaPostStatusMessage(url, token, session) {
-  let message = {},
-    tags = $("#smtags").val();
-
-  if (tags) {
-    tags = tags.replace(/#|\s/g, "")
-      .replace(/^(.*)/, "#$1")
-      .split(",")
-      .join(" #");
-    message.status_message = {
-      "text": $("#smtext").val() + "\n\n" + tags
-    };
-  } else {
-    message.status_message = {
-      "text": $("#smtext").val()
-    };
-  }
-
-  message.aspect_ids = $(".selectpicker").val();
-
-  $.ajax({
-      url: url + "/status_messages",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-        "Accept": "application/json, application/xhtml+xml",
-        "X-Requested-With": "XMLHttpRequest",
-        "X-CSRF-Token": token
-      },
-      data: message
-    })
-    .done((data, statusText, xhr) => {
-      if (xhr.status === 201) {
-        if (session === 2) {
-          browser.storage.local.set({
-            session: 0
-          });
-        }
-        notify("Post successfully sent to diaspora*", "success");
-        $("#send").prop("disabled", false).text("Send");
-      } else {
-        // On error try again for once
-        if (session < 2) {
-          browser.storage.local.set({
-            token: null
-          })
-          .then(sendRequestsToDiaspora("post message"));
-        } else {
-          let err = xhr.statusText;
-          if (xhr.status === 200) err = "Unauthorized";
-          onDiasporaError(err, "PostStatusMessage");
-        }
-      }
-    })
-    .fail(error => {
-      onDiasporaError(xhr.statusText, "PostStatusMessage");
-    });
-}
-
-function sendRequestsToDiaspora(req) {
-  browser.storage.local.get(["url", "username", "password", "token", "session"])
-    .then(items => {
-      if (!items.token) {
-        diasporaSignIn(items.url, items.username, items.password)
-          .then(response => {
-            if (response.error) {
-              onDiasporaError(response.error, "SignIn");
-            } else {
-              browser.storage.local.set({
-                  token: response.token,
-                  session: items.session + 1
-                })
-                .then(() => {
-                  if (req === "post message") {
-                    diasporaPostStatusMessage(items.url, response.token, items.session);
-                  } else {
-                    diasporaGetAspects(items.url);
-                  }
-                });
-            }
-          },
-          error => {
-            onDiasporaError(error, "SignIn");
-          });
-      } else {
-        if (req === "post message") {
-          diasporaPostStatusMessage(items.url, items.token, items.session);
-        } else {
-          diasporaGetAspects(items.url);
-        }
-      }
-    }, onStorageError);
-}
-
-// Custom buttons
-$("#smtext").markdown({
-  fullscreen: [{
-    enabled: false
-  }],
-  additionalButtons: [
-    [{
-      name: "groupCustom",
-      data: [{
-          name: "getAspects",
-          toggle: false,
-          title: "Get your aspects",
-          icon: {
-            fa: "fa fa-arrow-down"
-          },
-          btnText: "Get aspects",
-          btnClass: "btn btn-primary btn-sm",
-          btnType: "button",
-          callback: function(editor) {
-            editor.disableButtons("getAspects");
-            sendRequestsToDiaspora("get aspects");
-          }
-        },
-        {
-          name: "selectAspects",
-          toggle: false,
-          title: "Share with:",
-          icon: "",
-          btnText: "",
-          btnClass: "selectpicker btn-select",
-          btnType: "select"
-        }
-      ]
-    }]
-  ]
-});
-
-$(".selectpicker").selectpicker({
-  style: "btn-default btn-sm",
-  iconBase: "font-awesome",
-  tickIcon: "caret-down",
-  size: false
-});
-
-$("#send").click(function() {
-  $(this).prop("disabled", true).text("wait...");
-  sendRequestsToDiaspora("post message");
-});
-
-var port = browser.runtime.connect({
-  name: "from-publisher"
-});
-port.onMessage.addListener(msg => {
-  $("#smtext").val(msg);
-});
-
-browser.storage.local.get(["url", "username", "password", "aspects"])
-  .then(items => {
-    if (!items.url || !items.username || !items.password) {
-      notify("Please enter and save all required preferences before starting to share.", "warning");
-    } else {
-      if (items.aspects) updateAspectsSelector(items.aspects);
+    if (notify) {
+        onSuccess("Dropdown menu updated with your aspects.");
     }
-  }, onStorageError);
+}
+
+function storeAspects(aspects) {
+    try {
+        let aspectsObj = JSON.parse(aspects);
+
+        // Remove unnecessary items
+        $.each(aspectsObj, (index, value) => {
+            delete value.selected;
+        });
+
+        browser.storage.local.set({
+            aspects: aspectsObj
+        }).then(() => {
+            updateAspectsDropdown(aspectsObj, true);
+        });
+    } catch (e) {
+        onError("An error occurred while parsing the aspects list.");
+    }
+}
+
+function init() {
+    var diaspora = {},
+        send = function (payload = null) {
+            diaspora.retrieveToken()
+                .then(response => {
+                    if (response.error) {
+                        throw response.error;
+                    }
+                    if (payload) {
+                        return diaspora.postMessage(response.token, payload)
+                            .then(response => {
+                                if (response.error) {
+                                    throw response.error;
+                                }
+
+                                onSuccess(response.success);
+                            });
+                    } else {
+                        return diaspora.retrieveAspects()
+                            .then(response => {
+                                if (response.error) {
+                                    throw response.error;
+                                }
+
+                                storeAspects(response.aspects);
+                            });
+                    }
+                })
+                .catch(onError);
+        };
+
+    $editor.markdown({
+        fullscreen: [{
+            enabled: false
+        }],
+        additionalButtons: [
+            [{
+                name: "groupCustom",
+                data: [{
+                        name: "getAspects",
+                        toggle: false,
+                        title: "Get your aspects",
+                        icon: {
+                            fa: "fa fa-arrow-down"
+                        },
+                        btnText: "Get aspects",
+                        btnClass: "btn btn-primary",
+                        btnType: "button",
+                        callback: function (e) {
+                            e.disableButtons("all");
+                            e.setContent("Wait ...");
+                            send();
+                        }
+                    },
+                    {
+                        name: "selectAspects",
+                        toggle: true,
+                        icon: "",
+                        defaults: [{
+                            button: [{
+                                text: "All Aspects",
+                                icon: "fa-lock"
+                            }],
+                            menu: [{
+                                    id: "public",
+                                    text: "Public",
+                                    selected: false
+                                },
+                                {
+                                    id: "all_aspects",
+                                    text: "All aspects",
+                                    selected: true
+                            }]
+                        }],
+                        btnClass: "btn dropdown-toggle btn-dropdown",
+                        btnType: "dropdown"
+                    }
+                ]
+            }]
+        ],
+        addFooter: true,
+        onSend: function (e) {
+            var payload = e.getPostPayload();
+            if (payload) {
+                e.disableButtons("all");
+                e.setContent("Wait ...");
+                send(payload);
+            }
+        }
+    });
+
+    let storedData = browser.storage.local.get();
+
+    storedData.then(data => {
+        // Check if all required settings are entered
+        if (!data.url || !data.username || !data.password) {
+            $editor
+                .data("markdown")
+                .disableButtons("all");
+
+            onError("Please enter all required settings before starting to share.");
+        } else {
+            if (data.aspects) {
+                // Update the dropdown menu if the list of aspects is stored
+                updateAspectsDropdown(data.aspects);
+            }
+
+            diaspora = new diasporaAjax(data.url, data.username, data.password);
+
+            // Send a message to receive the content to be included in the editor
+            browser.runtime.sendMessage("getContent")
+                .then(response => {
+                    if (response.content) {
+                        $editor.val(response.content);
+                    }
+                }, onError);
+        }
+    });
+}
+
+init();
