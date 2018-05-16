ce8a70a68fb9b0e5d10ccd1cec2652c08ae0b671
arlogn <claudio@bastardi.net>
2018-05-16 20:19:12 +0200

    Fix markdown heading
    The syntax must have almost one space before the content

--- publisher/vendor/bootstrap-markdown/markdown.js
+++ publisher/vendor/bootstrap-markdown/markdown.js
@@ -683,7 +683,7 @@
   var Gruber = {
     block: {
       atxHeader: function atxHeader( block, next ) {
-        var m = block.match( /^(#{1,6})\s*(.*?)\s*#*\s*(?:\n|$)/ );
+        var m = block.match( /^(#{1,6})\s+(.*?)\s*#*\s*(?:\n|$)/ );
 
         if ( !m )
           return undefined;
